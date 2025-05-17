9c743188cb277545f973ceb24a3b5dabde6a6fc5
take27315 <takebanana25@gmail.com>
2025-05-17 21:53:46 +0900

    feat: add Firestore support and import it into main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
